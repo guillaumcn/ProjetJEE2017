75219577be06406b17bf3c97d18a1dc66d3f7ae8
NicoMiage <nicolas.kircun@gmail.com>
2017-12-14 15:42:42 +0100

    Ajout glassfish-ressources.xml

--- GestionProjetNTDP-ejb/build/generated-sources/ap-source-output/entity/ProjectClient_.java
+++ GestionProjetNTDP-ejb/build/generated-sources/ap-source-output/entity/ProjectClient_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-12T16:47:17")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-14T15:38:20")
 @StaticMetamodel(ProjectClient.class)
 public class ProjectClient_ { 
 
--- GestionProjetNTDP-ejb/build/generated-sources/ap-source-output/entity/Release_.java
+++ GestionProjetNTDP-ejb/build/generated-sources/ap-source-output/entity/Release_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-12T16:47:17")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-14T15:38:20")
 @StaticMetamodel(Release.class)
 public class Release_ { 
 
